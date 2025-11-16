bf644c287d25cf8ab40a63b1e9e16254b4c266ee
7minutech <elijohnson1107@gmail.com>
2025-11-16 16:06:47 -0600

    feat: ValidateJWT() for auth pkg

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/alexedwards/argon2id"
@@ -37,4 +38,25 @@ func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (str
 	}
 
 	return str, err
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claims := &jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(t *jwt.Token) (interface{}, error) {
+
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method")
+		}
+		return []byte(tokenSecret), nil
+	})
+	if err != nil || !token.Valid {
+		return uuid.UUID{}, fmt.Errorf("invalid token")
+	}
+
+	id, err := uuid.Parse(claims.Subject)
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	return id, nil
+
 }
